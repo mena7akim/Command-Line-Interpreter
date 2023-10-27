2e709580352e5a360c998b6943219cc583b0b293
mena7akim <102185230+mena7akim@users.noreply.github.com>
2023-10-27 12:04:07 +0200

    Added project files

---
+++ comandLineInterpreter/src/Parser.java
@@ -0,0 +1,15 @@
+public class Parser {
+    String commandName;
+    String[] args;
+    //This method will divide the input into commandName and args
+    //where "input" is the string command entered by the user
+    public boolean parse(String input){
+        return true;
+    }
+    public String getCommandName(){
+        return commandName;
+    }
+    public String[] getArgs(){
+        return args;
+    }
+}
---
+++ comandLineInterpreter/src/Terminal.java
@@ -0,0 +1,15 @@
+public class Terminal {
+    Parser parser;
+
+    //Implement each command in a method, for example:
+    public String pwd(){ return "c:/"; }
+    public void cd(String[] args){
+
+    }
+    // ...
+
+    //This method will choose the suitable command method to be called
+    public void chooseCommandAction(){
+
+    }
+}
